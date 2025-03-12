c6f0713850f9bbb2e9141c1b926a972298c20fbd
Kath0910 <katherinarivas10@gmail.com>
2025-03-11 21:42:44 -0300

    Create kath0910.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/kath0910.py
@@ -0,0 +1,14 @@
+# https://python.org
+
+#comentario en una linea
+
+"""
+esto también es un comentario en varias lineas
+"""
+'''hola mundo :)
+'''
+
+my_variable = "mi variable"
+my_variable = " nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante"  #por convención
