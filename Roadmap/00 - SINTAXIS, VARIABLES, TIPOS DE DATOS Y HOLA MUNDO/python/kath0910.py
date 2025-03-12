720a011fcee22998f0f54fac7d9da694bd768afe
Kath0910 <katherinarivas10@gmail.com>
2025-03-11 22:16:12 -0300

    kath0910.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/kath0910.py
@@ -0,0 +1,28 @@
+# http://python.org
+
+ # comentario en un a linea
+
+""" si existeee wiiii
+"""
+
+"""esto también es un
+comentario en avraiss lineas"""
+
+my_variable = "Mi variable"
+my_variable = "nuevo valor de mi variable"
+
+My_constant = "mi constante" # por convención
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+My_bool = False
+My_strring = "mi cadena de texto"
+
+print ("hola, python!")
+
+print(type(my_int))
+
+
+
+
